0efbec7b2c90c3c1e945557b2e6873a71e5d9a40
Maxime Gaide <maxime.gaide@ensma.fr>
2022-10-17 17:40:33 +0200

    work in progress — implement addLevel for ADD application type

--- src/main/java/fr/ensma/lias/jerboa/datastructures/MatchingTree.java
+++ src/main/java/fr/ensma/lias/jerboa/datastructures/MatchingTree.java
@@ -2,110 +2,104 @@ package fr.ensma.lias.jerboa.datastructures;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import up.jerboa.core.JerboaDart;
 import up.jerboa.core.JerboaRuleResult;
 
 public class MatchingTree {
 
-    // LevelOrbitMT root;
-    JerboaDart topoParameter; // the last dartID computed at the end of the matching process
+  // LevelOrbitMT root;
+  JerboaDart topoParameter; // the last dartID computed at the end of the
+                            // matching process
 
-    List<List<LevelEventMT>> leaves;
+  List<List<LevelEventMT>> leaves;
 
-    /**
-     * Data structure to match a history record against a GMap and build a model from a history
-     * record.
-     */
-    public MatchingTree() {
-        leaves = new ArrayList<>();
+  /**
+   * Data structure to match a history record against a GMap and build a model
+   * from a history record.
+   */
+  public MatchingTree() { leaves = new ArrayList<>(); }
+
+  public MatchingTree(JerboaRuleResult appResult, Application specEntry,
+                      HistoryRecord HR) {
+
+    leaves = new ArrayList<>();
+
+    // if spec is unedited
+
+    // for (LevelEventHR level : HR.getLeaf(0)) {
+    // // addLevel(level, spec, appResults);
+    // addLevel(level, specEntry, appResult);
+    // }
+
+    // else (spec is edited)
+    // -- op added
+    // -- op deleted
+    // -- op moved
+    // -- geometry modified (see if we should move it to `unedited` phase)
+  }
+
+  public JerboaDart getTopoParameter() { return topoParameter; }
+
+  public void addInitLevel(LevelEventHR level, Application prevApplication,
+                           Application curApplication,
+                           JerboaRuleResult appResult) {
+
+    ApplicationType appType = prevApplication.getApplicationType();
+    int appNumber = level.getAppNumber();
+    String nodeName = level.getNextLevelOrbitHR().getNodeName();
+
+    nodeNameToDartID(appNumber, nodeName, appResult, prevApplication);
+
+    LevelEventMT levelEvent =
+        new LevelEventMT(level.getAppNumber(), level.getEventList(), appType);
+    List<LevelEventMT> nextLeveleventMTs = Arrays.asList(levelEvent);
+
+    if (!leaves.isEmpty()) {
+      getLastLevel().get(0).getNextLevelOrbit().setNextLevelEventMTList(
+          nextLeveleventMTs);
     }
 
-    public MatchingTree(JerboaRuleResult appResult, Application specEntry, HistoryRecord HR) {
+    LevelOrbitMT levelOrbit =
+        new LevelOrbitMT(topoParameter.getID(),
+                         level.getNextLevelOrbitHR().getOrbitList(), null);
+    levelEvent.nextLevelOrbit = levelOrbit;
+    leaves.add(nextLeveleventMTs);
+  }
 
-        leaves = new ArrayList<>();
+  public void addAddLevel(Application application) {
+    // TODO: implement addLevel for ADD type -> NOMODIF only for the time being
+    return;
+  }
 
-        // if spec is unedited
+  /**
+   * Method to convert a nodeName from HR to an actual dart id (NID) the gmap
+   * can recognize when applying a rule
+   */
+  private void nodeNameToDartID(int AppNumber, String nodeName,
+                                JerboaRuleResult appResult,
+                                Application specEntry) {
 
-        // for (LevelEventHR level : HR.getLeaf(0)) {
-        // // addLevel(level, spec, appResults);
-        // addLevel(level, specEntry, appResult);
-        // }
-
-        // else (spec is edited)
-        // -- op added
-        // -- op deleted
-        // -- op moved
-        // -- geometry modified (see if we should move it to `unedited` phase)
-    }
-
-
-    public JerboaDart getTopoParameter() {
-        return topoParameter;
-    }
-
-    public void addInitLevel(LevelEventHR level, Application prevSpecEntry,
-            Application curSpecEntry, JerboaRuleResult appResult) {
-
-        // System.out.println("specentry:" + prevSpecEntry);
-
-        ApplicationType appType = prevSpecEntry.getApplicationType();
-        int appNumber = level.getAppNumber();
-        String nodeName = level.getNextLevelOrbitHR().getNodeName();
-
-        // nodeNameToDartID(appNumber, nodeName, appResult.get(appNumber), spec);
-        nodeNameToDartID(appNumber, nodeName, appResult, prevSpecEntry);
-
-        // in this case appType is *always* INIT
-        // ApplicationType appType = spec.getSpecEntry(appNumber).getAppType();
-
-        LevelEventMT levelEvent =
-                new LevelEventMT(level.getAppNumber(), level.getEventList(), appType);
-        List<LevelEventMT> nextLeveleventMTs = Arrays.asList(levelEvent);
-
-        if (!leaves.isEmpty()) {
-            getLastLevel().get(0).getNextLevelOrbit().setNextLevelEventMTList(nextLeveleventMTs);
+    int rowIndex = 0;
+    for (int i = 0; i < appResult.sizeCol(); i++) {
+      for (int j = 0; j < appResult.sizeRow(i); j++) {
+        if (appResult.get(i, j) == topoParameter) {
+          rowIndex = j;
         }
-
-        LevelOrbitMT levelOrbit = new LevelOrbitMT(topoParameter.getID(),
-                level.getNextLevelOrbitHR().getOrbitList(), null);
-        levelEvent.nextLevelOrbit = levelOrbit;
-        // root = levelOrbit;
-        leaves.add(nextLeveleventMTs);
-
-        // System.out.println(level + "\n" + levelEvent + "\n" + levelOrbit);
-
+      }
     }
 
-    /**
-     * Method to convert a nodeName from HR to an actual dart id (NID) the gmap can recognize when
-     * applying a rule
-     */
-    private void nodeNameToDartID(int AppNumber, String nodeName, JerboaRuleResult appResult,
-            Application specEntry) {
+    int colIndex = specEntry.getRule().getRightIndexRuleNode(nodeName);
+    // System.out.println("colIndex " + colIndex);
 
-        int rowIndex = 0;
-        for (int i = 0; i < appResult.sizeCol(); i++) {
-            for (int j = 0; j < appResult.sizeRow(i); j++) {
-                if (appResult.get(i, j) == topoParameter) {
-                    rowIndex = j;
-                }
-            }
-        }
+    // System.out.println(
+    // appResult.sizeCol() + " - " + colIndex + " - " + rowIndex + " - " +
+    // nodeName);
+    JerboaDart dart = appResult.get(colIndex, rowIndex);
+    this.topoParameter = dart;
+  }
 
-        int colIndex = specEntry.getRule().getRightIndexRuleNode(nodeName);
-        // System.out.println("colIndex " + colIndex);
-
-        // System.out.println(
-        // appResult.sizeCol() + " - " + colIndex + " - " + rowIndex + " - " + nodeName);
-        JerboaDart dart = appResult.get(colIndex, rowIndex);
-        this.topoParameter = dart;
-    }
-
-    private List<LevelEventMT> getLastLevel() {
-        return leaves.get(leaves.size() - 1);
-    }
-
+  private List<LevelEventMT> getLastLevel() {
+    return leaves.get(leaves.size() - 1);
+  }
 }

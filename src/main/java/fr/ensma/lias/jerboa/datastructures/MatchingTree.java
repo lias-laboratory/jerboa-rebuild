17c906c48ac3174c85b186eaaef750fdb0eb6bf8
Maxime Gaide <maxime.gaide@ensma.fr>
2022-10-26 18:00:50 +0200

    Fix bad merge

--- src/main/java/fr/ensma/lias/jerboa/datastructures/MatchingTree.java
+++ src/main/java/fr/ensma/lias/jerboa/datastructures/MatchingTree.java
@@ -13,123 +13,18 @@ import up.jerboa.core.rule.JerboaRuleNode;
 
 public class MatchingTree {
 
-<<<<<<< HEAD
-=======
 	// LevelOrbitMT root;
->>>>>>> 470bd1e (Added application are partly supported - cleaning is needed urgently - do not launch DemoRejeuID it will not work as it is behind)
 	JerboaDart topoParameter; // the last dartID computed at the end of the matching process
 	List<List<LevelEventMT>> leaves;
 
 	/**
-<<<<<<< HEAD
-	 * Data structure to match a history record against a GMap and build a model from a history
-=======
 	 * * Data structure to match a history record against a GMap and build a model from a history *
->>>>>>> 470bd1e (Added application are partly supported - cleaning is needed urgently - do not launch DemoRejeuID it will not work as it is behind)
 	 * record.
 	 */
 	public MatchingTree() {
 		leaves = new ArrayList<>();
 	}
 
-<<<<<<< HEAD
-	public JerboaDart getTopoParameter() {
-		return topoParameter;
-	}
-
-	/**
-	 * Add a level to a matching tree from a history record by matching a rule application against a
-	 * GMAP. This method allow to compute the topological parameters and reevaluate a model
-	 *
-	 * @param levelEventHR A given history record level used to compute an application's parameters
-	 * @param prevApplication An application matched in the previous iteration
-	 * @param curApplication Current application to match
-	 * @param prevAppResult Array containing the jerboa darts filtered or created by the previous
-	 *        application's rule
-	 */
-	public void addLevel(LevelEventHR levelEventHR, Application prevApplication,
-			Application curApplication, JerboaRuleResult prevAppResult) {
-
-		ApplicationType appType = prevApplication.getApplicationType();
-		int appNumber = levelEventHR.getAppNumber();
-		String nodeName = levelEventHR.getNextLevelOrbit().getNodeName();
-
-		// // JerboaRebuiltRule prevRule = (JerboaRebuiltRule) prevApplication.getRule();
-		// LevelOrbitHR levelOrbitHR = levelEventHR.getNextLevelOrbit();
-
-		// for (NodeOrbitHR nodeOrbitHR : levelOrbitHR.getOrbitList()) {
-		// JerboaRuleNode curRuleNode =
-		// prevRule.getRightRuleNode(prevRule.getRightIndexRuleNode(nodeName));
-		// for (Link child : nodeOrbitHR.getChildren()) {
-		// // Event event = child.child.event;
-		// List<JerboaRuleNode> ruleNodesOrbitList = JerboaRuleNode.orbit(curRuleNode.))
-		// switch (child.child.event) {
-		// case
-		// }
-		// }
-		// }
-
-		// NOTE: working with identical results for the time being
-		//
-		// For each levelEventHR check its matching type (i.e. identical, edited or notmatched)
-		//
-		// How to check for Identical ?
-		//
-		// --> from LevelEventHR check nodename·<nodeOrbit> matches levelEventHR's nodeEvents
-		// ----> if it's a match then preserve current topological parameter
-		// ----> if it's not a match then apply a default strategy (for later)
-		//
-		// In case of added operation
-		//
-		// For each levelEventHR check its matching type
-		//
-		// --> from LevelEventHR use node·<nodeOrbit> orbits to compute events
-		// ----> if its a noeffect or nomodif preserve current topological parameter
-		// ----> if its something else then apply relevant strategy (deleted, splitted or merge
-		// darts)
-
-		nodeNameToDartID(appNumber, nodeName, prevAppResult, prevApplication);
-
-		LevelEventMT levelEvent =
-				new LevelEventMT(levelEventHR.getAppNumber(), levelEventHR.getEventList(), appType);
-		List<LevelEventMT> nextLeveleventMTs = Arrays.asList(levelEvent);
-
-		if (!leaves.isEmpty()) {
-			getLastLevel().get(0).getNextLevelOrbit().setNextLevelEventMTList(nextLeveleventMTs);
-		}
-
-		LevelOrbitMT levelOrbit = new LevelOrbitMT(topoParameter.getID(),
-				levelEventHR.getNextLevelOrbit().getOrbitList(), null);
-		levelEvent.nextLevelOrbit = levelOrbit;
-		leaves.add(nextLeveleventMTs);
-	}
-
-	/**
-	 * Convert a node name to an actual dart id (NID) the gmap can recognize when applying a rule
-	 */
-	private void nodeNameToDartID(int AppNumber, String nodeName, JerboaRuleResult appResult,
-			Application application) {
-
-		int rowIndex = 0;
-		for (int i = 0; i < appResult.sizeCol(); i++) {
-			for (int j = 0; j < appResult.sizeRow(i); j++) {
-				if (appResult.get(i, j) == topoParameter) {
-					rowIndex = j;
-				}
-			}
-		}
-
-		int colIndex = application.getRule().getRightIndexRuleNode(nodeName);
-		// System.out.println("colIndex " + colIndex);
-
-		// System.out.println(
-		// appResult.sizeCol() + " - " + colIndex + " - " + rowIndex + " - " +
-		// nodeName);
-		JerboaDart dart = appResult.get(colIndex, rowIndex);
-		this.topoParameter = dart;
-	}
-
-=======
 	public MatchingTree(JerboaRuleResult appResult, Application specEntry, HistoryRecord HR) {
 
 		leaves = new ArrayList<>();
@@ -161,7 +56,7 @@ public class MatchingTree {
 		if (application.getApplicationType() != ApplicationType.ADD) {
 
 			int appNumber = levelEventHR.getAppNumber();
-			String nodeName = levelEventHR.getNextLevelOrbitHR().getNodeName();
+			String nodeName = levelEventHR.getNextLevelOrbit().getNodeName();
 
 			nodeNameToDartID(appNumber, nodeName, appResult, application);
 
@@ -172,7 +67,7 @@ public class MatchingTree {
 					levelEventHR.getEventList(), appType);
 
 			levelOrbitMT = new LevelOrbitMT(topoParameter.getID(),
-					levelEventHR.getNextLevelOrbitHR().getOrbitList(), null);
+					levelEventHR.getNextLevelOrbit().getOrbitList(), null);
 			levelEventMT.nextLevelOrbit = levelOrbitMT;
 
 			// leaves.add(Arrays.asList(levelEventMT));
@@ -296,7 +191,6 @@ public class MatchingTree {
 		this.topoParameter = dart;
 	}
 
->>>>>>> 470bd1e (Added application are partly supported - cleaning is needed urgently - do not launch DemoRejeuID it will not work as it is behind)
 	private List<LevelEventMT> getLastLevel() {
 		return leaves.get(leaves.size() - 1);
 	}

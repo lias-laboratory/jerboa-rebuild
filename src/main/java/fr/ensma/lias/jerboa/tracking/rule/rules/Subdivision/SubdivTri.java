6aafeec8d1a6faf4a2a3d40454ddb6aa5d37f54c
Maxime Gaide <maxime.gaide@ensma.fr>
2023-04-18 13:20:21 +0200

    add example + subvidision rules in subdivision category

---
+++ src/main/java/fr/ensma/lias/jerboa/tracking/rule/rules/Subdivision/SubdivTri.java
@@ -0,0 +1,113 @@
+package fr.ensma.lias.jerboa.tracking.rule.rules.Subdivision;
+
+
+import java.util.List;
+import java.util.ArrayList;
+import up.jerboa.core.rule.*;
+import up.jerboa.core.util.*;
+import up.jerboa.core.*;
+import up.jerboa.exception.JerboaException;
+
+import fr.ensma.lias.jerboa.tracking.rule.rules.RawDynaOrTrackModeler;
+import fr.ensma.lias.jerboa.embeddings.Vec3;
+import java.awt.Color;
+
+
+
+/**
+ * 
+ */
+
+
+
+public class SubdivTri extends JerboaRuleGenerated {
+
+    private transient JerboaRowPattern curleftPattern;
+
+
+	// BEGIN PARAMETERS Transformed 
+
+
+	// END PARAMETERS 
+
+
+
+    public SubdivTri(RawDynaOrTrackModeler modeler) throws JerboaException {
+
+        super(modeler, "SubdivTri", "Subdivision");
+
+        // -------- LEFT GRAPH
+        JerboaRuleNode ln0 = new JerboaRuleNode("n0", 0, JerboaOrbit.orbit(0,1,2,3), 3);
+        left.add(ln0);
+        hooks.add(ln0);
+
+        // -------- RIGHT GRAPH
+        JerboaRuleNode rn0 = new JerboaRuleNode("n0", 0, JerboaOrbit.orbit(0,-1,2,3), 3);
+        JerboaRuleNode rn1 = new JerboaRuleNode("n1", 1, JerboaOrbit.orbit(-1,2,-1,3), 3);
+        JerboaRuleNode rn2 = new JerboaRuleNode("n2", 2, JerboaOrbit.orbit(1,2,-1,3), 3, new SubdivTriExprRn2pos());
+        right.add(rn0);
+        right.add(rn1);
+        right.add(rn2);
+        rn0.setAlpha(1, rn1);
+        rn1.setAlpha(0, rn2);
+;
+        // ------- SPECIFIED FEATURE
+        computeEfficientTopoStructure();
+        computeSpreadOperation();
+        // ------- USER DECLARATION 
+    }
+
+    public int reverseAssoc(int i) {
+        switch(i) {
+        case 0: return 0;
+        case 1: return -1;
+        case 2: return -1;
+        }
+        return -1;
+    }
+
+    public int attachedNode(int i) {
+        switch(i) {
+        case 0: return 0;
+        case 1: return 0;
+        case 2: return 0;
+        }
+        return -1;
+    }
+
+    public JerboaRuleResult applyRule(JerboaGMap gmap, JerboaDart n0) throws JerboaException {
+        JerboaInputHooksGeneric ____jme_hooks = new JerboaInputHooksGeneric();
+        ____jme_hooks.addCol(n0);
+        return applyRule(gmap, ____jme_hooks);
+	}
+
+    private class SubdivTriExprRn2pos implements JerboaRuleExpression {
+
+        @Override
+        public Object compute(JerboaGMap gmap, JerboaRuleOperation rule,JerboaRowPattern leftPattern, JerboaRuleNode rulenode) throws JerboaException {
+            curleftPattern = leftPattern;
+// ======== BEGIN CODE TRANSLATION FOR EXPRESSION COMPUTATION
+            // ======== SEPARATION CODE TRANSLATION FOR EXPRESSION COMPUTATION
+Vec3 res = new Vec3();
+res.barycenter(gmap.<fr.ensma.lias.jerboa.embeddings.Vec3>collect(curleftPattern.getNode(0),JerboaOrbit.orbit(0,1),0));
+return res;
+// ======== END CODE TRANSLATION FOR EXPRESSION COMPUTATION
+        }
+
+        @Override
+        public String getName() {
+            return "pos";
+        }
+
+        @Override
+        public int getEmbedding() {
+            return ((RawDynaOrTrackModeler)modeler).getPos().getID();
+        }
+    }
+
+    // Facility for accessing to the dart
+    private JerboaDart n0() {
+        return curleftPattern.getNode(0);
+    }
+
+} // end rule Class

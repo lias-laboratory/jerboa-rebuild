7123eef605243a8ec4e6a49e0bcb5cad7911cfd7
Maxime Gaide <maxime.gaide@ensma.fr>
2023-11-29 15:10:36 +0100

    experimenting with scripts for future script-compatible reevaluation

---
+++ src/main/java/fr/ensma/lias/jerboa/experiments/MesScriptsRejeu.java
@@ -0,0 +1,64 @@
+package fr.ensma.lias.jerboa.experiments;
+
+import fr.ensma.lias.jerboa.core.rule.rules.Subdivision.SubdivQuad;
+import fr.ensma.lias.jerboa.core.rule.rules.Subdivision.SubdivQuadFake;
+import fr.ensma.lias.jerboa.core.rule.rules.Subdivision.SubdivTri;
+import fr.ensma.lias.jerboa.core.rule.rules.Subdivision.SubdivTriFake;
+import fr.ensma.lias.jerboa.embeddings.Vec3;
+import java.util.List;
+import up.jerboa.core.JerboaGMap;
+import up.jerboa.core.JerboaInputHooks;
+import up.jerboa.core.JerboaModeler;
+import up.jerboa.core.JerboaRuleResult;
+import up.jerboa.core.rule.JerboaRowPattern;
+import up.jerboa.exception.JerboaException;
+
+/** MesScriptsRejeu */
+public class MesScriptsRejeu {
+
+  public static JerboaRuleResult exe1(
+      JerboaModeler modeler, JerboaGMap gmap, JerboaInputHooks hooks) {
+    System.out.println("HELLO");
+    int res = Vec3.askInt("0 faux, autre vrai", "0");
+    SubdivQuadFake sqf = (SubdivQuadFake) modeler.getRule("SubdivQuadFake");
+    SubdivTri st = (SubdivTri) modeler.getRule("SubdivTri");
+
+    SubdivQuad sq = (SubdivQuad) modeler.getRule("SubdivQuad");
+    SubdivTriFake stf = (SubdivTriFake) modeler.getRule("SubdivTriFake");
+    List<JerboaRowPattern> leftPattern;
+
+    if (res != 0) {
+      try {
+        sqf.apply(gmap, hooks);
+      } catch (JerboaException e) {
+        e.printStackTrace();
+      }
+      leftPattern = (List<JerboaRowPattern>) sqf.getFakeLeft();
+      for (JerboaRowPattern row : leftPattern) {
+        System.out.println(row);
+      }
+      try {
+        st.apply(gmap, hooks);
+      } catch (JerboaException e) {
+        e.printStackTrace();
+      }
+    } else {
+      try {
+        stf.apply(gmap, hooks);
+      } catch (JerboaException e) {
+        e.printStackTrace();
+      }
+      leftPattern = (List<JerboaRowPattern>) stf.getFakeLeft();
+      for (JerboaRowPattern row : leftPattern) {
+        System.out.println(row);
+      }
+      try {
+        sq.apply(gmap, hooks);
+      } catch (JerboaException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return null;
+  }
+}

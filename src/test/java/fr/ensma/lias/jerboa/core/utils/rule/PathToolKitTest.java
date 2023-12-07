90819c92e59e403c1998550ae4d4171ab6fd4f04
Maxime Gaide <maxime.gaide@ensma.fr>
2023-12-07 18:04:20 +0100

    wip: add getNextInstance to PathToolKit

--- src/test/java/fr/ensma/lias/jerboa/core/utils/rule/PathToolKitTest.java
+++ src/test/java/fr/ensma/lias/jerboa/core/utils/rule/PathToolKitTest.java
@@ -1,7 +1,9 @@
-package fr.ensma.lias.jerboa.core.utils.rules;
+package fr.ensma.lias.jerboa.core.utils.rule;
+
+import static org.junit.Assert.assertEquals;
 
 import fr.ensma.lias.jerboa.core.rule.rules.ModelerGenerated;
-import fr.ensma.lias.jerboa.core.utils.rule.PathToolKit;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
 import up.jerboa.core.JerboaOrbit;
@@ -13,15 +15,44 @@ import up.jerboa.exception.JerboaException;
 public class PathToolKitTest {
 
   @Test
-  public void test_Case() throws JerboaException {
+  public void test_split_2_TriangulateFace() throws JerboaException {
     ModelerGenerated modeler = new ModelerGenerated();
     JerboaRuleGenerated rule = (JerboaRuleGenerated) modeler.getRule("TriangulateFace");
     JerboaRuleNode node =
         rule.getRightRuleNode(rule.getRightIndexRuleNode(rule.getHooks().get(0).getName()));
-    JerboaOrbit orbitType = JerboaOrbit.orbit(0, 1, 3);
+    JerboaOrbit orbitType = JerboaOrbit.orbit(0, 1);
     int splitLink = 2;
 
     List<Integer> list = PathToolKit.getNextInstancePath(rule, node, splitLink, orbitType);
     System.out.println(list);
+    assertEquals(Arrays.asList(0, 1, 2, 1), list);
+  }
+
+  @Test
+  public void test_split_1_InsertEdge() throws JerboaException {
+    ModelerGenerated modeler = new ModelerGenerated();
+    JerboaRuleGenerated rule = (JerboaRuleGenerated) modeler.getRule("InsertEdge");
+    JerboaRuleNode node =
+        rule.getRightRuleNode(rule.getRightIndexRuleNode(rule.getHooks().get(0).getName()));
+    JerboaOrbit orbitType = JerboaOrbit.orbit(0, 1);
+    int splitLink = 2;
+
+    List<Integer> list = PathToolKit.getNextInstancePath(rule, node, splitLink, orbitType);
+    System.out.println(list);
+    assertEquals(Arrays.asList(1, 2, 1), list);
+  }
+
+  @Test
+  public void test_split_2_InsertVertexFolded() throws JerboaException {
+    ModelerGenerated modeler = new ModelerGenerated();
+    JerboaRuleGenerated rule = (JerboaRuleGenerated) modeler.getRule("InsertVertexFolded");
+    JerboaRuleNode node =
+        rule.getRightRuleNode(rule.getRightIndexRuleNode(rule.getHooks().get(0).getName()));
+    JerboaOrbit orbitType = JerboaOrbit.orbit(0);
+    int splitLink = 1;
+
+    List<Integer> list = PathToolKit.getNextInstancePath(rule, node, splitLink, orbitType);
+    System.out.println(list);
+    assertEquals(Arrays.asList(0, 1, 0), list);
   }
 }

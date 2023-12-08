3265ffbf678ab605b3a208cb85a2f1c2821e2e92
Maxime Gaide <maxime.gaide@ensma.fr>
2023-12-08 17:03:32 +0100

    update doosabin

--- src/main/java/fr/ensma/lias/jerboa/core/rule/rules/Subdivision/DooSabin.java
+++ src/main/java/fr/ensma/lias/jerboa/core/rule/rules/Subdivision/DooSabin.java
@@ -44,18 +44,18 @@ public class DooSabin extends JerboaRebuiltRule {
         hooks.add(ln0);
 
         // -------- RIGHT GRAPH
-        JerboaRuleNode rn0 = new JerboaRuleNode("n0", 0, JerboaOrbit.orbit(0,1,-1), 3);
-        JerboaRuleNode rn1 = new JerboaRuleNode("n1", 1, JerboaOrbit.orbit(0,-1,-1), 3);
-        JerboaRuleNode rn2 = new JerboaRuleNode("n2", 2, JerboaOrbit.orbit(-1,-1,0), 3);
-        JerboaRuleNode rn3 = new JerboaRuleNode("n3", 3, JerboaOrbit.orbit(-1,1,0), 3);
+        JerboaRuleNode rn0 = new JerboaRuleNode("n0", 0, JerboaOrbit.orbit(0,1,-1), 3, new DooSabinExprRn0pos());
+        JerboaRuleNode rn2 = new JerboaRuleNode("n2", 1, JerboaOrbit.orbit(-1,-1,0), 3);
+        JerboaRuleNode rn3 = new JerboaRuleNode("n3", 2, JerboaOrbit.orbit(-1,1,0), 3);
+        JerboaRuleNode rn1 = new JerboaRuleNode("n1", 3, JerboaOrbit.orbit(0,-1,-1), 3);
         right.add(rn0);
-        right.add(rn1);
         right.add(rn2);
         right.add(rn3);
+        right.add(rn1);
+        rn2.setAlpha(2, rn3);
         rn0.setAlpha(2, rn1);
         rn1.setAlpha(1, rn2);
         rn1.setAlpha(3, rn1);
-        rn2.setAlpha(2, rn3);
         rn2.setAlpha(3, rn2);
         rn3.setAlpha(3, rn3);
 ;
@@ -90,6 +90,41 @@ public class DooSabin extends JerboaRebuiltRule {
         ____jme_hooks.addCol(n0);
         return applyRule(gmap, ____jme_hooks);
 	}
+
+    private class DooSabinExprRn0pos implements JerboaRuleExpression {
+
+        @Override
+        public Object compute(JerboaGMap gmap, JerboaRuleOperation rule,JerboaRowPattern leftPattern, JerboaRuleNode rulenode) throws JerboaException {
+            curleftPattern = leftPattern;
+// ======== BEGIN CODE TRANSLATION FOR EXPRESSION COMPUTATION
+            // ======== SEPARATION CODE TRANSLATION FOR EXPRESSION COMPUTATION
+java.util.List<fr.ensma.lias.jerboa.embeddings.Vec3> l = new ArrayList<fr.ensma.lias.jerboa.embeddings.Vec3>();
+Vec3 centerFace = new Vec3();
+centerFace.barycenter(gmap.<fr.ensma.lias.jerboa.embeddings.Vec3>collect(curleftPattern.getNode(0),JerboaOrbit.orbit(0,1),0));
+l.add(centerFace);
+Vec3 midEdge1 = new Vec3();
+midEdge1.barycenter(gmap.<fr.ensma.lias.jerboa.embeddings.Vec3>collect(curleftPattern.getNode(0),JerboaOrbit.orbit(0),0));
+l.add(midEdge1);
+Vec3 midEdge2 = new Vec3();
+midEdge2.barycenter(gmap.<fr.ensma.lias.jerboa.embeddings.Vec3>collect(curleftPattern.getNode(0).alpha(1),JerboaOrbit.orbit(0),0));
+l.add(midEdge2);
+l.add(curleftPattern.getNode(0).<fr.ensma.lias.jerboa.embeddings.Vec3>ebd(0));
+Vec3 res = new Vec3();
+res.barycenter(l);
+return res;
+// ======== END CODE TRANSLATION FOR EXPRESSION COMPUTATION
+        }
+
+        @Override
+        public String getName() {
+            return "pos";
+        }
+
+        @Override
+        public int getEmbedding() {
+            return ((ModelerGenerated)modeler).getPos().getID();
+        }
+    }
 
     // Facility for accessing to the dart
     private JerboaDart n0() {

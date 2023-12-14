cd6fb70a9fa70accf658d3884f5a3dd74d824a9f
Maxime Gaide <maxime.gaide@ensma.fr>
2023-12-14 15:52:12 +0100

    wip

--- src/main/java/fr/ensma/lias/jerboa/experiments/ScriptConditionalReevaluation.java
+++ src/main/java/fr/ensma/lias/jerboa/experiments/ScriptConditionalReevaluation.java
@@ -1,10 +1,15 @@
 package fr.ensma.lias.jerboa.experiments;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import up.jerboa.core.JerboaDart;
+import up.jerboa.core.rule.JerboaRowPattern;
 import up.jerboa.core.util.JerboaRuleGenerated;
+import up.jerboa.core.util.Pair;
 
 class ScriptConditionalReevaluation {
+
   /**
    * @param scriptEntry The dart used as parameter for the script
    * @param ruleAEntryPath The path to the dart used as entry for ruleA
@@ -13,14 +18,38 @@ class ScriptConditionalReevaluation {
    * @param ruleB A rule called during Reevaluation instead of ruleA
    * @return //TODO initializeMatching specify return
    */
-  void initializeMatching(
+  private void initializeMatching(
       JerboaDart scriptEntry,
       List<Integer> ruleAEntryPath,
       List<Integer> ruleBEntryPath,
       JerboaRuleGenerated ruleA,
       JerboaRuleGenerated ruleB) {
-    // TODO: match ruleA leftPattern against ruleB leftpattern and deduce matching instanciation in
-    // ruleB
+
+    // get ruleA left row pattern from ruleA
+    List<JerboaRowPattern> leftRowPatternA = new ArrayList<>();
+    // get ruleB left row pattern from ruleB
+    List<JerboaRowPattern> leftRowPatternB = new ArrayList<>();
+
+    // TODO get Dart from ruleAEntryPath
+    JerboaDart dartA = null;
+    // coords for dart A in row pattern A
+    Pair<Integer, Integer> coordA1 = new Pair<Integer, Integer>(-1, -1);
+    // coords for dart A in row pattern B
+    Pair<Integer, Integer> coordA2 = new Pair<Integer, Integer>(-1, -1);
+    // TODO get Dart from ruleBEntryPath
+    // coords for dart B in row pattern A
+    Pair<Integer, Integer> coordB1 = new Pair<Integer, Integer>(-1, -1);
+    // coords for dart B in row pattern B
+    Pair<Integer, Integer> coordB2 = new Pair<Integer, Integer>(-1, -1);
+    JerboaDart dartB = null;
+
+    for (int i = 0; i < leftRowPatternA.size(); i++) {
+      for (int j = 0; j < leftRowPatternA.get(i).size(); j++) {
+        if (leftRowPatternA.get(i).get(j).equals(dartA)) {}
+        // TODO assign coords to coordA…
+        break;
+      }
+    }
   }
 
   /**
@@ -29,9 +58,17 @@ class ScriptConditionalReevaluation {
    * @param ruleB A rule called during Reevaluation instead of ruleA
    * @return //TODO matchOrbits specify return
    */
-  void matchOrbits(int matchInstance, JerboaRuleGenerated ruleA, JerboaRuleGenerated ruleB) {
+  private void matchOrbits(
+      int matchInstance, JerboaRuleGenerated ruleA, JerboaRuleGenerated ruleB) {
     // TODO: match orbits from ruleA onto ruleB
-    // TODO: orbits must be matched by grouping: creation => Generative; split, merge, modification
+    // TODO: orbits must be matched by grouping: creation => Generative; split,
+    // merge, modification
     // => Modification; deletion => Deletion
+  }
+
+  public JerboaDart conditionalReevaluation() {
+    initializeMatching(null, null, null, null, null);
+    matchOrbits((Integer) null, null, null);
+    return null;
   }
 }
